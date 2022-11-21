453ebbde3c2919ef077d9292a55b725aa928159f
lcelik <lidijacelik87@gmail.com>
2022-11-21 19:50:54 +0000

    adding Employee project with J Unit(testing). Also SQL is implemented

---
+++ MyLayeredEmployeeJDBCWithJUnitProject/src/com/sujata/test/EmployeeDaoTest.java
@@ -0,0 +1,53 @@
+package com.sujata.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.sujata.entity.Employee;
+import com.sujata.persistence.EmployeeDaoImpl;
+
+class EmployeeDaoTest {
+	
+	EmployeeDaoImpl employeeDaoImpl;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		employeeDaoImpl=new EmployeeDaoImpl();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		employeeDaoImpl=null;
+	}
+
+	@Test
+	void testGetAllRecords() {
+		assertTrue(employeeDaoImpl.getAllRecords().size()>0);
+	}
+
+	@Test
+	void testSearchRecord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInsertRecord() {
+		assertEquals(1, employeeDaoImpl.insertRecord(new Employee(108,"HHHH","Executive","Sales",34000,LocalDate.now())));
+	}
+
+	@Test
+	void testDeleteRecord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateSalary() {
+		fail("Not yet implemented");
+	}
+
+}
